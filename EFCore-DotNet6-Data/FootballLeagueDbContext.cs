628cbdc5e5182485ba2c930ab8a0bf38d712281b
Goomni <rnals622@gmail.com>
2021-11-28 23:48:50 +0900

    Adding EF Core migration, Updating Database, Adding POCO

---
+++ EFCore-DotNet6-Data/FootballLeagueDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EFCore_DotNet6_Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EFCore_DotNet6_Data
+{
+    public class FootballLeagueDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeague_EF_Core")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information );
+        }
+        public DbSet<Team>? Teams { get; set; }
+        public DbSet<League>? Leagues { get; set; }
+    }
+}
---
+++ EFCore-DotNet6-Domain/Team.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCore_DotNet6_Domain
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int LeagueId { get; set; } 
+        public virtual League? League { get; set; }
+
+    }
+}
